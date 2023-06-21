958f1e63626f993811e114eb6c662dd98be042bf
Edu <info.die@gmail.com>
2023-06-21 11:32:29 -0600

    update menu.js

--- scripts/menu.js
+++ scripts/menu.js
@@ -2,15 +2,22 @@ const menuBtn = document.getElementById("menu-btn");
 const closeBtn = document.getElementById("close-menu-btn");
 const navMenu = document.getElementById("nav-menu");
 
-menuBtn.onclick= () => {
-    navMenu.classList.replace("nav-menu","nav-mobile")
-    menuBtn.style.display="none"
-    closeBtn.classList.replace("none", "close-menu-btn")
+const navItems = [...document.getElementsByClassName("nav-item")];
+
+const navClassesToggle = ["nav-mobile", "nav-menu"];
+const closeClassesToggle = ["close-menu-btn", "none"];
+const menuBtnStyleToggle = "inline";
+
+const navMenuHandler = (navClasses, closeClasses, menuBtnStyle) => {
+    navMenu.classList.replace(navClasses[0], navClasses[1]);
+    menuBtn.style.display = menuBtnStyle;
+    closeBtn.classList.replace(closeClasses[0], closeClasses[1]);
 }
 
 
-closeBtn.onclick = () =>{
-    navMenu.classList.replace("nav-mobile","nav-menu")
-    menuBtn.style.display="inline"
-    closeBtn.classList.replace( "close-menu-btn", "none")
-}
+navItems.forEach(element => {
+    element.onclick = () => navMenuHandler(navClassesToggle.reverse(), closeClassesToggle.reverse(), menuBtnStyleToggle)
+});
+
+menuBtn.onclick = () => navMenuHandler(navClassesToggle.reverse(), closeClassesToggle.reverse(), "none");
+closeBtn.onclick = () => navMenuHandler(navClassesToggle.reverse(), closeClassesToggle.reverse(), menuBtnStyleToggle);
