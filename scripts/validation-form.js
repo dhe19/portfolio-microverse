75ed614bbda0f23760281f147ca552a3893cad2f
Edu <info.die@gmail.com>
2023-06-24 02:22:51 -0600

    create validation forms js to check contact form

---
+++ scripts/validation-form.js
@@ -0,0 +1,27 @@
+const form = document.getElementById('contact-form');
+const email = document.getElementById('email');
+const submitBtn = document.getElementById('submit-btn');
+let inserted = false;
+
+const errorMessage = (message) => {
+    const error = document.createElement('p');
+    error.textContent = message;
+    if (!inserted)
+        submitBtn.insertAdjacentElement('beforebegin', error);
+    inserted = true;
+}
+
+form.onsubmit = (event) => {
+
+    event.preventDefault();
+
+    if (email.value !== email.value.toLowerCase()) {
+        errorMessage("Email must be in lower case")
+    }
+
+    else {
+        form.submit();
+    }
+}
+
+
